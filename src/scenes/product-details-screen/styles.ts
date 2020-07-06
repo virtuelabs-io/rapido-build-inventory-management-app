d02d8ae44f8a6ee5462ef29cd9efdcd4c3d7aafe
Anirup Patnaik <anirup049@gmail.com>
2020-07-07 03:40:04 +0530

    Product Details setup in Detail component

--- src/scenes/product-details-screen/styles.ts
+++ src/scenes/product-details-screen/styles.ts
@@ -1,10 +1,23 @@
 import { StyleSheet } from 'react-native';
+import Utils from '@virtuelabs-io/rapido-modules/src/commons/utils';
 
 const Styles = StyleSheet.create({
     screen: {
         flex: 1,
         paddingHorizontal: "5%",
         paddingVertical: 10,
+    },
+    pointsCard: {
+        flexDirection: 'row'
+    },
+    pointsTitle: {
+        marginVertical: 7,
+        fontSize: 22,
+        fontFamily: Utils.getTabTitleFontFamily()
+    },
+    points: {
+        marginBottom: 10,
+        marginHorizontal: 10
     }
 })
 
--- src/scenes/product-details-screen/view.tsx
+++ src/scenes/product-details-screen/view.tsx
@@ -1,11 +1,14 @@
 import React, { Dispatch } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import Styles from './styles'
 import { ProductDetailsScreenProps, ProductDetailsScreenState, ProductDetailsScreenDispatchProps } from './types'
 import { getStackStyles } from '../../commons/styles';
 import { AppState, AppActionTypes } from '../../store';
 import { connect } from 'react-redux';
 import { RTitleText } from '@virtuelabs-io/rapido-modules/src/components/atoms/r-title-text/view';
+import { Card } from '@virtuelabs-io/rapido-modules/src/components/atoms/card/view';
+import { Entypo } from '@expo/vector-icons';
+import { RText } from '@virtuelabs-io/rapido-modules/src/components/atoms/r-text/view';
 
 class ProductDetailsScreen extends React.Component<ProductDetailsScreenProps, ProductDetailsScreenState> {
     constructor(props: ProductDetailsScreenProps, state: ProductDetailsScreenState) {
@@ -16,19 +19,31 @@ class ProductDetailsScreen extends React.Component<ProductDetailsScreenProps, Pr
             () => {
                 // @ts-ignore
                 // REASON: state picked up from redux
-                
-             }
+
+            }
         ))
     }
 
-    
 
-    
+
+
 
     render(): React.ReactNode {
         return (
             <ScrollView style={Styles.screen}>
                 <RTitleText>{this.props.data.name}</RTitleText>
+                <Card>
+                    <Text style={Styles.pointsTitle}>Product Details</Text>
+                    {this.props.data.points.map((point) => {
+                        return (
+                            <View style={Styles.pointsCard}>
+                                <Entypo name="dot-single" size={24} color="black" />
+                                <Text style={Styles.points}>{point}</Text>
+                            </View>
+                        )
+                    })
+                    }
+                </Card>
             </ScrollView>
         )
     }
@@ -36,6 +51,7 @@ class ProductDetailsScreen extends React.Component<ProductDetailsScreenProps, Pr
 
 const mapStatetoProps = (state: AppState, localProps: ProductDetailsScreenProps): ProductDetailsScreenProps => {
     var selProduct = state.products.headerRecords.filter((product) => product.id == localProps.route.params.id)
+    console.log(selProduct[0].fields.points)
     return {
         ...localProps,
         data: selProduct[0].fields
