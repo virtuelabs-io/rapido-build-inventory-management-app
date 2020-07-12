392f2fa3141be5548e038fb6bf1867584ac16e45
Anirup Patnaik <anirup049@gmail.com>
2020-07-12 06:10:19 +0530

    Adding Animations to images

--- src/components/organisms/animated-view/types.ts
+++ src/components/organisms/animated-view/types.ts
@@ -2,11 +2,11 @@ import { Image, Animated } from "react-native"
 
 export type AnimatedViewProps = {
     values: any,
-    
     image: string,
     // animatedValue: any
 }
 
 export type AnimatedViewState = {
     animation: Animated.Value;
+    showCancel: boolean
 }
--- src/components/organisms/animated-view/view.tsx
+++ src/components/organisms/animated-view/view.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View,Easing, StyleSheet,Image, FlatList, Animated, TouchableOpacity } from 'react-native';
+import { Text, View, Easing, StyleSheet, Image, FlatList, Animated, TouchableOpacity, ImageBackground, TouchableHighlight, Alert } from 'react-native';
 import { AnimatedViewState, AnimatedViewProps } from './types';
 import Styles from './styles'
 
@@ -7,39 +7,55 @@ class AnimatedView extends React.Component<AnimatedViewProps, AnimatedViewState>
     animation = Animated.Value
     constructor(props: AnimatedViewProps) {
         super(props);
-        this.state = { animation: new Animated.Value(0) };
+        this.state = { animation: new Animated.Value(0), showCancel: false };
+
     }
 
     startAnimation() {
         console.log('animation started')
         const animations = [
-            Animated.timing(this.state.animation, {toValue: 1.0, duration: 150, easing: Easing.linear, useNativeDriver: true}),
+            Animated.timing(this.state.animation, { toValue: 1.0, duration: 150, easing: Easing.linear, useNativeDriver: true }),
             // rotate in other direction, to minimum value (= twice the duration of above)
-            Animated.timing(this.state.animation, {toValue: -1.0, duration: 300, easing: Easing.linear, useNativeDriver: true}),
+            Animated.timing(this.state.animation, { toValue: -1.0, duration: 300, easing: Easing.linear, useNativeDriver: true }),
             // return to begin position
-            Animated.timing(this.state.animation, {toValue: 0.0, duration: 150, easing: Easing.linear, useNativeDriver: true})
+            Animated.timing(this.state.animation, { toValue: 0.0, duration: 150, easing: Easing.linear, useNativeDriver: true })
         ];
         Animated.sequence(animations).start()
+
+        Alert.alert(
+            "Information",
+            "Delete Image",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => console.log("Image Delete") }
+            ],
+            { cancelable: false }
+          );
     }
 
     render() {
         const { values } = this.props;
         return (
-            <Animated.View style={{ margin: 10, transform: [{ 
-                // scale: this.state.animation,
-                rotate: this.state.animation.interpolate({
-                    inputRange: [-1, 1],
-                    outputRange: ['-0.1rad', '0.1rad']
-                  })
-            }] }}>
-                <TouchableOpacity onPress={() => this.startAnimation()}>
-                <Image 
+            <Animated.View style={{
+                margin: 10, transform: [{
+                    // scale: this.state.animation,
+                    rotate: this.state.animation.interpolate({
+                        inputRange: [-1, 1],
+                        outputRange: ['-0.1rad', '0.1rad']
+                    })
+                }]
+            }}>
+                <TouchableOpacity onLongPress={() => this.startAnimation()}>
+                    <Image 
                     source={{uri: this.props.image}}
-                    style={Styles.image} 
-                />
+                    style={Styles.image}/>
                 </TouchableOpacity>
             </Animated.View>
-        );
+        )
     }
 }
 
